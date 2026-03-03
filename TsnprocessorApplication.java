965f8a8eb71c0c618950c5d1882439de6e23e56d
Vishnu Bhakare <109420037+vishnubhakare2000@users.noreply.github.com>
2026-03-03 16:32:15 +0530

    New class added
    New class added

---
+++ TsnprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.ex.fm.tsnprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsnprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsnprocessorApplication.class, args);
+	}
+
+}
